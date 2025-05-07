74cdb7c3d3806589c60f170119ed3b124798e589
evanbacon <bacon@expo.io>
2025-05-07 12:28:01 -0500

    Add new HomeScreen component and update dependencies; remove unused tab layout files

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,20 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { ReanimatedScreenProvider } from "react-native-screens/reanimated";
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
 
   if (!loaded) {
@@ -18,11 +23,27 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <ReanimatedScreenProvider>
+        <Stack>
+          <Stack.Screen
+            name="index"
+            options={{
+              title: "Hello",
+              headerTransparent: true,
+              headerBlurEffect: "systemChromeMaterial",
+              headerLargeTitleShadowVisible: false,
+              headerLargeStyle: {
+                backgroundColor: "transparent",
+              },
+              headerLargeTitle: true,
+              // Hide small header shadow for continuous blur
+              headerShadowVisible: false,
+            }}
+          />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+      </ReanimatedScreenProvider>
       <StatusBar style="auto" />
     </ThemeProvider>
   );
---
+++ app/index.tsx
@@ -0,0 +1,113 @@
+import { StyleSheet, Text } from "react-native";
+
+import Masked from "@react-native-masked-view/masked-view";
+import { BlurView } from "expo-blur";
+import Animated, {
+  interpolate,
+  useAnimatedRef,
+  useAnimatedStyle,
+  useScrollViewOffset,
+} from "react-native-reanimated";
+import { useReanimatedHeaderHeight } from "react-native-screens/reanimated";
+
+import * as AC from "@bacons/apple-colors";
+
+const AnimMask = Animated.createAnimatedComponent(Masked);
+
+export default function HomeScreen() {
+  const ref = useAnimatedRef();
+  const scroll = useScrollViewOffset(ref);
+  const style = useAnimatedStyle(() => ({
+    opacity: interpolate(scroll.value, [-152, -20], [0, 1], "clamp"),
+  }));
+  console.log("scroll", scroll.value);
+
+  return (
+    <>
+      <Animated.ScrollView
+        ref={ref}
+        contentContainerStyle={{ paddingTop: 48 }}
+        contentInsetAdjustmentBehavior={"automatic"}
+      >
+        {new Array(20).fill(0).map((_, index) => (
+          <Text
+            key={index}
+            style={{
+              padding: 16,
+              margin: 8,
+              backgroundColor: AC.secondarySystemGroupedBackground,
+              borderRadius: 8,
+              color: AC.label,
+              textAlign: "center",
+              fontSize: 24,
+              fontWeight: "bold",
+              fontFamily:
+                process.env.EXPO_OS === "ios" ? "ui-rounded" : undefined,
+            }}
+          >
+            Step {index + 1}
+          </Text>
+        ))}
+      </Animated.ScrollView>
+
+      <Glur direction="top" style={style} />
+    </>
+  );
+}
+
+function Glur({ direction, style }: { direction: "top" | "bottom" }) {
+  const headerHeight = useReanimatedHeaderHeight();
+  return (
+    <AnimMask
+      maskElement={
+        <Animated.View
+          style={[
+            {
+              position: "absolute",
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: 0,
+              backgroundColor: "transparent",
+              experimental_backgroundImage: `linear-gradient(to ${direction}, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 1) 75%)`,
+            },
+            style,
+          ]}
+        />
+      }
+      style={{
+        position: "absolute",
+        top: headerHeight,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        height: 96,
+      }}
+    >
+      <BlurView
+        intensity={100}
+        tint={"systemChromeMaterial"}
+        style={StyleSheet.absoluteFill}
+      />
+    </AnimMask>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: "absolute",
+  },
+});
